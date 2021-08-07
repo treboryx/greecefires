e56b96c1e52ffd308a2c615fef4fafe26651df96
Robert <treboryx@gmail.com>
2021-08-07 21:18:29 +0300

    remove geolocation

--- plugins/google-maps.js
+++ plugins/google-maps.js
@@ -1,8 +1,6 @@
 import Vue from "vue";
 
 import * as VueGoogleMaps from "@/node_modules/vue2-google-maps";
-import VueGeolocation from "vue-browser-geolocation";
-Vue.use(VueGeolocation);
 
 Vue.use(VueGoogleMaps, {
   load: {
